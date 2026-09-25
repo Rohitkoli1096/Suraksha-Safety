f4c3f4cf7830a6df25211e70ab057939b2624485
Rohit Koli <rohitkoli1096@gmail.com>
2026-09-25 20:13:51 +0530

    Initial commit - Suraksha Safety

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-13942bd7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mvmmv3otd0o"
+    "revision": "0.8eq2idep16"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
